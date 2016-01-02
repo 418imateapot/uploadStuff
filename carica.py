915bd2b2ac64991b3b9db2c648e8ba30b3ebe3f9
patmha <mybrandnumail@mail.com>
2016-01-02 22:56:27 +0100

    Update carica.py

--- carica.py
+++ carica.py
@@ -7,34 +7,38 @@ import json
 
 import requests
 
-## l'xpath si riferisce a rivista di statistica
-## todo: mettere un case e a seconda che sia dlib, statistica o altro scegliere l'xpath corrispondente
-##
+# dato un URL di un'issue di dlib o statistica crea un json con titoli e URL degli articoli di quel numero
 def getURLandTitle(url):
 	page = requests.get(url)
 	tree = html.fromstring(page.content)
 
 	res = []
-	for i,a in enumerate(tree.xpath('//div[@class="tocTitle"]//a')):
-		res.append({"id": a.attrib.get('href').split('/').pop(-1), "url": a.attrib.get('href'), "title": a.text})
-
+	if "dlib" in url:
+		pref = url.rsplit("/", 1).pop(0) + "/" # ad esempio http://www.dlib.org/dlib/november14/	
+		for i,a in enumerate(tree.xpath("(//p[@class='contents']/a)[position()<last()]")): ### dlib nov14
+			res.append({"url": pref + a.attrib.get('href'), "title": a.text})
+	else: # significa che e' rivista di statistica 
+		for i,a in enumerate(tree.xpath('//div[@class="tocTitle"]//a')):
+			res.append({"url": a.attrib.get('href'), "title": a.text})
 	return res
 
 
-## in input prende un json con chiavi: url, title, id.
-## todo: fare modifiche in modo che funzioni indipendentemente dalla rivista
-## adesso e' corretto solo per statistica. in teoria dovrei usare prefisso+id invece dell'url
+## in input prende un json con chiavi: url e title.
 def createTriples(someJson):
-	rivstat = "http://rivista-statistica.unibo.it/article/view/"
 	retv = ""
 	for obj in someJson:
-		retv += "<" + rivstat + obj["id"] + ">" + "\n\ta\tfabio:Work ;\n"
-		retv += "\tfabio:hasPortrayal\n\t\t<" + obj["url"] + ".html> ;\n"
-		retv += "\tfrbr:realization\n\t\t<" + obj["url"] + "_ver1> .\n\n"
-		retv += "<" + obj["url"] + "_ver1>\n\ta\tfabio:Expression ;\n"
-		retv += "\tfabio:hasRepresentation\n\t\t<" + obj["url"] + "> .\n\n"
-		retv += "<" + obj["url"] + ">\n\ta\tfabio:Item .\n\n"
-		retv += "<" + obj["url"] + "_ver1>\n\tdcterms:title \"" + obj["title"] + "\"^^xmls:string .\n\n"
+		print "current object: " + str(obj)
+		objPref = obj["url"] if "statistica" in obj["url"] else obj["url"].rsplit(".", 1).pop(0)
+		#print "cur obj has prefix" + objPref
+		#print "\n"
+
+		retv += "<" + objPref + ">" + "\n\ta\tfabio:Work ;\n"
+		retv += "\tfabio:hasPortrayal\n\t\t<" + objPref + ".html> ;\n"
+		retv += "\tfrbr:realization\n\t\t<" + objPref + "_ver1> .\n\n"
+		retv += "<" + objPref + "_ver1>\n\ta\tfabio:Expression ;\n"
+		retv += "\tfabio:hasRepresentation\n\t\t<" + objPref + ".html> .\n\n"
+		retv += "<" + objPref + ".html>\n\ta\tfabio:Item .\n\n"
+		retv += "<" + objPref + "_ver1>\n\tdcterms:title \"" + obj["title"] + "\"^^xmls:string .\n\n"
 	return retv
 
 
@@ -56,7 +60,8 @@ def createTriples(someJson):
 # <http://www.dlib.org/dlib/november14/beel/11beel_ver1>
 # 	dcterms:title "The Architecture and Datasets of Docear's Research Paper Recommender System"^^xmls:string .
 
+print createTriples(getURLandTitle("http://rivista-statistica.unibo.it/issue/view/467?acceptCookies=1")).encode('utf-8')
+#print "\n\n\n\naaaaaaaaa\n\n"
+print createTriples(getURLandTitle("http://www.dlib.org/dlib/november14/11contents.html")).encode('utf-8')
 
-
-resJson = getURLandTitle("http://rivista-statistica.unibo.it/issue/view/467?acceptCookies=1")
-print createTriples(resJson).encode('utf-8')
+print createTriples(getURLandTitle("http://rivista-statistica.unibo.it/issue/view/514")).encode('utf-8')
